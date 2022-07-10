f4d314ea0ab2d85df5f70ab5d7d3345311d54fc0
Scorpion <74263158+kunaltak606@users.noreply.github.com>
2022-07-10 07:11:59 -0700

    initial

---
+++ qb-cityhall/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-CityHall'
+version '2.0.0'
+
+ui_page 'html/index.html'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'config.lua'
+}
+
+server_script 'server/main.lua'
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    'client/main.lua'
+}
+
+files {
+    'html/*.js',
+    'html/*.html',
+    'html/*.css'
+}
+
+lua54 'yes'
+use_fxv2_oal 'yes'
